97620a44b1c5d9c8bb5152dfe8a53c7adbe3a1a0
Andrey Akishin <Andrey@Akishin.cc>
2012-12-03 12:10:42 +0400

    HSTJ-17: Implement Map.toQueryString() function

--- src/main/java/ru/histone/evaluator/functions/node/object/ToQueryString.java
+++ src/main/java/ru/histone/evaluator/functions/node/object/ToQueryString.java
@@ -15,18 +15,19 @@
  */
 package ru.histone.evaluator.functions.node.object;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import ru.histone.Histone;
 import ru.histone.evaluator.functions.node.NodeFunction;
 import ru.histone.evaluator.nodes.Node;
 import ru.histone.evaluator.nodes.NodeFactory;
 import ru.histone.evaluator.nodes.ObjectHistoneNode;
-import ru.histone.evaluator.nodes.ObjectHistoneNode;
-import ru.histone.evaluator.nodes.StringHistoneNode;
 
 /**
- * Transforms key to parameter map to query string. <br/>
+ * Transforms key-to-parameter map to query string. <br/>
  */
 public class ToQueryString extends NodeFunction<ObjectHistoneNode> {
 
@@ -41,14 +42,44 @@ public class ToQueryString extends NodeFunction<ObjectHistoneNode> {
 
     @Override
 	public Node execute(ObjectHistoneNode target, Node... args) {
+		if (args.length > 2) {
+			Histone.runtime_log_warn("Function toQueryString() can have one argument, but you provided '{}' arguments", args.length);
+			return getNodeFactory().UNDEFINED;
+		}
+		final String keyPrefix = args.length > 0 && !args[0].isNull() ? args[0].getAsString().getValue() : null;
+		final String separator = args.length > 1 ? args[1].getAsString().getValue() : "&";
 		Set<Entry<Object, Node>> entries = target.getElements().entrySet();
 		StringBuilder b = new StringBuilder();
 		for (Entry<Object, Node> entry : entries) {
-			final String key = entry.getKey().toString();
-			final String value = entry.getValue().getAsString().getValue();
-			b.append("&").append(key).append("=").append(value);
+			String key = entry.getKey().toString();
+			// if key is integer then it is just a index in array. Add given
+			// prefix before
+			key = entry.getKey() instanceof Integer && keyPrefix != null ? keyPrefix + key : key;
+			addValue(keyPrefix, separator, b, key, entry.getValue());
 		}
 		String result = entries.size() == 0 ? "" : b.substring(1);
 		return getNodeFactory().string(result);
+	}
+
+	private void addValue(final String keyPrefix, final String separator, final StringBuilder b, final String key, Node node) {
+		if (node.isObject()) {
+			Set<Entry<Object, Node>> entries = node.getAsObject().getElements().entrySet();
+			for (Entry<Object, Node> entry : entries) {
+				String newKey = entry.getKey().toString();
+				newKey = key + "[" + newKey + "]";
+				addValue(keyPrefix, separator, b, newKey, entry.getValue());
+			}
+		} else {
+			if (node.isUndefined())
+				return;
+			String value = node.getAsString().getValue();
+			try {
+				value = URLEncoder.encode(value, "UTF-8").replace("+", "%20");
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			b.append(separator).append(key).append("=").append(value);
+		}
 	}
 }
