aca8f88e86f34357ad8484eea586fae1dcba03e5
Nikolay Izotov <nizotov@gmail.com>
2012-12-26 18:25:36 +0400

    HSTJ-21: formating of url moved to PathUtils class

--- src/main/java/ru/histone/evaluator/functions/global/ResolveURI.java
+++ src/main/java/ru/histone/evaluator/functions/global/ResolveURI.java
@@ -17,29 +17,12 @@ package ru.histone.evaluator.functions.global;
 
 import ru.histone.evaluator.nodes.Node;
 import ru.histone.evaluator.nodes.NodeFactory;
-import ru.histone.utils.StringUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import ru.histone.utils.PathUtils;
 
 /**
  * Returns the URI to be resolved against given URI <br/>
  */
 public class ResolveURI extends GlobalFunction {
-
-    public static final String URL_DIRNAME_REGEXP = "^(.*)\\/";
-    public static final String URL_PARSER_REGEXP = "^(?:([^:\\/?\\#]+):)?(?:\\/\\/([^\\/?\\#]*))?([^?\\#]*)(?:\\?([^\\#]*))?(?:\\#(.*))?";
-    private static final String SLASH = "/";
-    private static final String QUESTION = "?";
-    private static final String DOUBLE_SLASH = "//";
-    private static final String COLON = ":";
-    private static final String HASH = "#";
-    private static final String DOT = ".";
-    private static final String DOUBLE_DOT = "..";
-    private static final char CHAR_SLASH = '/';
-    private static final String EMPTY = "";
 
     public ResolveURI(NodeFactory nodeFactory) {
         super(nodeFactory);
@@ -52,133 +35,7 @@ public class ResolveURI extends GlobalFunction {
 
     @Override
     public Node execute(Node... args) throws GlobalFunctionExecutionException {
-        String rel = args[0].getAsString().getValue();
-        String base = args[1].getAsString().getValue();
-        StringBuilder result = new StringBuilder();
-
-        URI relUri = parseURI(rel);
-        URI baseUri = parseURI(base);
-
-        if (StringUtils.isNotEmpty(relUri.getScheme())) {
-            result.append((relUri.getScheme() + COLON));
-            if (StringUtils.isNotEmpty(relUri.getAuthority())) {
-                result.append((DOUBLE_SLASH + relUri.getAuthority()));
-            }
-            if (StringUtils.isNotEmpty(removeDotSegments(relUri.getPath()))) {
-                result.append(removeDotSegments(relUri.getPath()));
-            }
-            if (StringUtils.isNotEmpty(relUri.getQuery())) {
-                result.append((QUESTION + relUri.getQuery()));
-            }
-        } else {
-            if (StringUtils.isNotEmpty(baseUri.getScheme())) {
-                result.append((baseUri.getScheme() + COLON));
-            }
-            if (StringUtils.isNotEmpty(relUri.getAuthority())) {
-                result.append((DOUBLE_SLASH + relUri.getAuthority()));
-                if (StringUtils.isNotEmpty(removeDotSegments(relUri.getPath()))) {
-                    result.append(removeDotSegments(relUri.getPath()));
-                }
-                if (StringUtils.isNotEmpty(relUri.getQuery())) {
-                    result.append((QUESTION + relUri.getQuery()));
-                }
-            } else {
-                if (StringUtils.isNotEmpty(baseUri.getAuthority())) {
-                    result.append((DOUBLE_SLASH + baseUri.getAuthority()));
-                }
-                if (StringUtils.isNotEmpty(relUri.getPath())) {
-                    StringBuilder pathStringBuilder = new StringBuilder();
-                    if (relUri.getPath().charAt(0) == CHAR_SLASH) {
-                        pathStringBuilder.append(relUri.getPath());
-                    } else {
-                        if (StringUtils.isNotEmpty(baseUri.getAuthority()) && StringUtils.isEmpty(baseUri.getPath())) {
-                            pathStringBuilder.append(SLASH);
-                        } else {
-                            pathStringBuilder.append(resolvePath(baseUri.getPath()));
-                        }
-                        pathStringBuilder.append(relUri.getPath());
-                    }
-                    String resolvedPath = removeDotSegments(pathStringBuilder.toString());
-                    if (StringUtils.isNotEmpty(resolvedPath)) {
-                        result.append(resolvedPath);
-                    }
-                    if (StringUtils.isNotEmpty(relUri.getQuery())) {
-                        result.append((QUESTION + relUri.getQuery()));
-                    }
-                } else {
-                    if (StringUtils.isNotEmpty(baseUri.getPath())) {
-                        result.append(baseUri.getPath());
-                    }
-                    if (StringUtils.isNotEmpty(relUri.getQuery())) {
-                        result.append((QUESTION + relUri.getQuery()));
-                    } else if (StringUtils.isNotEmpty(baseUri.getQuery())) {
-                        result.append((QUESTION + baseUri.getQuery()));
-                    }
-                }
-            }
-        }
-        if (StringUtils.isNotEmpty(relUri.getFragment())) {
-            result.append((HASH + relUri.getFragment()));
-        }
-        return getNodeFactory().string(result.toString());
-    }
-
-    private String removeDotSegments(String path) {
-        String[] splitedPath = path.split(SLASH);
-        if (splitedPath.length < 1) {
-            return path;
-        }
-        boolean isAbsolute = StringUtils.isEmpty(splitedPath[0]);
-        List<String> result = new ArrayList<String>();
-        String fragment = EMPTY;
-        int startIndex = isAbsolute ? 1 : 0;
-        for (int i = startIndex; i < splitedPath.length; i++) {
-            fragment = splitedPath[i];
-            if (fragment.equals(DOUBLE_DOT)) {
-                if (result.size() >= 1) {
-                    result.remove(result.size() - 1);
-                }
-            } else if (!fragment.equals(DOT)) {
-                result.add(fragment);
-            }
-        }
-        if (fragment.equals(DOT) || fragment.equals(DOUBLE_DOT)) {
-            result.add(EMPTY);
-        }
-        StringBuilder builder = new StringBuilder();
-        for (String element : result) {
-            builder.append(SLASH).append(element);
-        }
-        if (!isAbsolute && builder.length() > 0) {
-            builder.deleteCharAt(0);
-        }
-        if (path.endsWith(SLASH) && builder.charAt(builder.length() - 1) != CHAR_SLASH) {
-            builder.append(SLASH);
-        }
-        return builder.toString();
-    }
-
-    private String resolvePath(String path) {
-        Pattern parser = Pattern.compile(URL_DIRNAME_REGEXP);
-        Matcher matcher = parser.matcher(path);
-        if (matcher.find()) {
-            return matcher.group(0);
-        }
-        return EMPTY;
-    }
-
-    private URI parseURI(String uriString) {
-        Pattern parser = Pattern.compile(URL_PARSER_REGEXP);
-        Matcher matcher = parser.matcher(uriString);
-        URI uri = new URI();
-
-        if (matcher.find()) {
-            uri.setScheme(matcher.group(1));
-            uri.setAuthority(matcher.group(2));
-            uri.setPath(matcher.group(3));
-            uri.setQuery(matcher.group(4));
-            uri.setFragment(matcher.group(5));
-        }
-        return uri;
+        String result = PathUtils.resolveUrl(args[0].getAsString().getValue(), args[1].getAsString().getValue());
+        return getNodeFactory().string(result);
     }
 }
---
+++ src/main/java/ru/histone/utils/PathUtils.java
@@ -0,0 +1,171 @@
+/**
+ *    Copyright 2012 MegaFon
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package ru.histone.utils;
+
+
+import ru.histone.evaluator.functions.global.URI;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PathUtils {
+
+    public static final String URL_DIRNAME_REGEXP = "^(.*)\\/";
+    public static final String URL_PARSER_REGEXP = "^(?:([^:\\/?\\#]+):)?(?:\\/\\/([^\\/?\\#]*))?([^?\\#]*)(?:\\?([^\\#]*))?(?:\\#(.*))?";
+    private static final String SLASH = "/";
+    private static final String QUESTION = "?";
+    private static final String DOUBLE_SLASH = "//";
+    private static final String COLON = ":";
+    private static final String HASH = "#";
+    private static final String DOT = ".";
+    private static final String DOUBLE_DOT = "..";
+    private static final char CHAR_SLASH = '/';
+    private static final String EMPTY = "";
+    
+   
+    public static String resolveUrl(String relativeUrl, String baseUrl)  {
+
+        StringBuilder result = new StringBuilder();
+
+        URI relUri = parseURI(relativeUrl);
+        URI baseUri = parseURI(baseUrl);
+
+        if (StringUtils.isNotEmpty(relUri.getScheme())) {
+            result.append((relUri.getScheme() + COLON));
+            if (StringUtils.isNotEmpty(relUri.getAuthority())) {
+                result.append((DOUBLE_SLASH + relUri.getAuthority()));
+            }
+            if (StringUtils.isNotEmpty(removeDotSegments(relUri.getPath()))) {
+                result.append(removeDotSegments(relUri.getPath()));
+            }
+            if (StringUtils.isNotEmpty(relUri.getQuery())) {
+                result.append((QUESTION + relUri.getQuery()));
+            }
+        } else {
+            if (StringUtils.isNotEmpty(baseUri.getScheme())) {
+                result.append((baseUri.getScheme() + COLON));
+            }
+            if (StringUtils.isNotEmpty(relUri.getAuthority())) {
+                result.append((DOUBLE_SLASH + relUri.getAuthority()));
+                if (StringUtils.isNotEmpty(removeDotSegments(relUri.getPath()))) {
+                    result.append(removeDotSegments(relUri.getPath()));
+                }
+                if (StringUtils.isNotEmpty(relUri.getQuery())) {
+                    result.append((QUESTION + relUri.getQuery()));
+                }
+            } else {
+                if (StringUtils.isNotEmpty(baseUri.getAuthority())) {
+                    result.append((DOUBLE_SLASH + baseUri.getAuthority()));
+                }
+                if (StringUtils.isNotEmpty(relUri.getPath())) {
+                    StringBuilder pathStringBuilder = new StringBuilder();
+                    if (relUri.getPath().charAt(0) == CHAR_SLASH) {
+                        pathStringBuilder.append(relUri.getPath());
+                    } else {
+                        if (StringUtils.isNotEmpty(baseUri.getAuthority()) && StringUtils.isEmpty(baseUri.getPath())) {
+                            pathStringBuilder.append(SLASH);
+                        } else {
+                            pathStringBuilder.append(resolvePath(baseUri.getPath()));
+                        }
+                        pathStringBuilder.append(relUri.getPath());
+                    }
+                    String resolvedPath = removeDotSegments(pathStringBuilder.toString());
+                    if (StringUtils.isNotEmpty(resolvedPath)) {
+                        result.append(resolvedPath);
+                    }
+                    if (StringUtils.isNotEmpty(relUri.getQuery())) {
+                        result.append((QUESTION + relUri.getQuery()));
+                    }
+                } else {
+                    if (StringUtils.isNotEmpty(baseUri.getPath())) {
+                        result.append(baseUri.getPath());
+                    }
+                    if (StringUtils.isNotEmpty(relUri.getQuery())) {
+                        result.append((QUESTION + relUri.getQuery()));
+                    } else if (StringUtils.isNotEmpty(baseUri.getQuery())) {
+                        result.append((QUESTION + baseUri.getQuery()));
+                    }
+                }
+            }
+        }
+        if (StringUtils.isNotEmpty(relUri.getFragment())) {
+            result.append((HASH + relUri.getFragment()));
+        }
+        return result.toString();
+    }
+
+    private static String removeDotSegments(String path) {
+        String[] splitedPath = path.split(SLASH);
+        if (splitedPath.length < 1) {
+            return path;
+        }
+        boolean isAbsolute = StringUtils.isEmpty(splitedPath[0]);
+        List<String> result = new ArrayList<String>();
+        String fragment = EMPTY;
+        int startIndex = isAbsolute ? 1 : 0;
+        for (int i = startIndex; i < splitedPath.length; i++) {
+            fragment = splitedPath[i];
+            if (fragment.equals(DOUBLE_DOT)) {
+                if (result.size() >= 1) {
+                    result.remove(result.size() - 1);
+                }
+            } else if (!fragment.equals(DOT)) {
+                result.add(fragment);
+            }
+        }
+        if (fragment.equals(DOT) || fragment.equals(DOUBLE_DOT)) {
+            result.add(EMPTY);
+        }
+        StringBuilder builder = new StringBuilder();
+        for (String element : result) {
+            builder.append(SLASH).append(element);
+        }
+        if (!isAbsolute && builder.length() > 0) {
+            builder.deleteCharAt(0);
+        }
+        if (path.endsWith(SLASH) && builder.charAt(builder.length() - 1) != CHAR_SLASH) {
+            builder.append(SLASH);
+        }
+        return builder.toString();
+    }
+
+    private static String resolvePath(String path) {
+        Pattern parser = Pattern.compile(URL_DIRNAME_REGEXP);
+        Matcher matcher = parser.matcher(path);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        return EMPTY;
+    }
+
+    private static URI parseURI(String uriString) {
+        Pattern parser = Pattern.compile(URL_PARSER_REGEXP);
+        Matcher matcher = parser.matcher(uriString);
+        URI uri = new URI();
+
+        if (matcher.find()) {
+            uri.setScheme(matcher.group(1));
+            uri.setAuthority(matcher.group(2));
+            uri.setPath(matcher.group(3));
+            uri.setQuery(matcher.group(4));
+            uri.setFragment(matcher.group(5));
+        }
+        return uri;
+    }
+
+}
